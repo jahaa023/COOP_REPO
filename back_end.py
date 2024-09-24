852b74971e1720b78e7ad739ab23373b3e075a79
unknown <jakob.johannes.hansgaard@outlook.com>
2024-09-24 11:41:57 +0200

    La til deling

--- back_end.py
+++ back_end.py
@@ -17,3 +17,9 @@ def gange():
     tall2 = input("Skriv inn det andre tallet: ")
     sum = int(tall1)*int(tall2)
     print(f'{tall1} ganger {tall2} er lik {sum}')
+
+def dele():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    sum = int(tall1)/(tall2)
+    print(f'{tall1} delt på {tall2} er lik {sum}')
