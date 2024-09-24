9f3f0ab65dc4971925d638ce89010a830ef28aa8
FiskFisk <julian.m.lund@gmail.com>
2024-09-24 12:48:01 +0200

    Fixed Front-end to match back-end

--- front_end.py
+++ front_end.py
@@ -1,12 +1,11 @@
-from back_end import *
-
+from back_end import kalkulator
 
 def printMeny():
     print("------------------- Kalkulator -------------------")
     print("| 1. Legg sammen (pluss)                         |")
     print("| 2. Trekk fra   (minus)                         |")
-    print("| 3. Gange       (***TODO***)                    |")
-    print("| 4. Dele        (***TODO***)                    |")
+    print("| 3. Gange       (gange)                         |")
+    print("| 4. Dele        (dele)                          |")
     print("| 5. Avslutt                                     |")
     print("--------------------------------------------------")
     menyvalg = input("Velg operasjon fra menyen: ")
@@ -15,18 +14,20 @@ def printMeny():
 
 def utfoerMenyvalg(valgtTall):
     if valgtTall == "1":
-        leggSammen()
+        kalkulator(0)
         pause_og_fortsett()
     elif valgtTall == "2":
-        trekkFra()
+        kalkulator(1)
         pause_og_fortsett()
     elif valgtTall == "3":
-        gange()
+        kalkulator(2)
+        pause_og_fortsett()
     elif valgtTall == "4":
-        unfinished()
+        kalkulator(3)
+        pause_og_fortsett()
     elif valgtTall == "5":
         bekreftelse = input("Er du sikker på at du vil avslutte? J/N ")
-        if (bekreftelse == "J" or bekreftelse == "j"):
+        if bekreftelse.lower() == "j":
             exit()
         else:
             printMeny()
@@ -37,10 +38,12 @@ def utfoerMenyvalg(valgtTall):
         printMeny()
 
 
+
 def pause_og_fortsett():
     input("-- Trykk en tast for å fortsette --")
     printMeny()
 
 
 printMeny()
+
 
