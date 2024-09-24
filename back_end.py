18595a59eb20105330383066dfcafbc917a72ae1
unknown <jakob.johannes.hansgaard@outlook.com>
2024-09-24 13:12:22 +0200

    nummere får desimaler når man deler, input sjekk om nummer skjer i frontend

--- back_end.py
+++ back_end.py
@@ -1,17 +1,4 @@
-def sjekk_om_nummer(andre_forste):
-    while True:
-        try:
-            userInput = int(input(f'Skriv inn det {andre_forste} tallet: '))       
-        except ValueError:
-            print("Ugyldig tall")
-            continue
-        else:
-            break
-    return userInput
-
-def kalkulator(type):
-    tall1 = sjekk_om_nummer("første")
-    tall2 = sjekk_om_nummer("andre")
+def kalkulator(type, tall1, tall2):
     if type == 0:
         result = int(tall1) + int(tall2)
         symbol = " pluss "
@@ -22,6 +9,6 @@ def kalkulator(type):
         result = int(tall1)*int(tall2)
         symbol = " ganger "
     else:
-        result = round(int(tall1)/int(tall2))
+        result = int(tall1)/int(tall2)
         symbol = " delt på "
     print(f'{tall1}{symbol}{tall2} er lik {result}')
