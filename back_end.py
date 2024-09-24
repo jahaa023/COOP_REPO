9630eef17e75dc6a4b31bddf06effb90b404406b
unknown <jakob.johannes.hansgaard@outlook.com>
2024-09-24 11:39:56 +0200

    La til ganging

--- back_end.py
+++ back_end.py
@@ -12,7 +12,8 @@ def trekkFra():
     print(f'{tall2} - {tall2} = {diff}')
 
 
-''' Her er noen påbegynte tanker til utvidelse
 def gange():
-    produkt = 3*3
-    TODO: Gjøre ferdig '''
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    sum = int(tall1)*int(tall2)
+    print(f'{tall1} ganger {tall2} er lik {sum}')
